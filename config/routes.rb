d16d3b03949b459aeb5df6ecdcbbb61547bde82c
kodmm <tawaratsumida.kosei.infratop@gmail.com>
2020-02-16 04:42:17 +0000

    created end_users_devise yet admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'end_users/show'
+  get 'end_users/edit'
+  get 'end_users/confirm'
+  devise_for :admins
+  devise_for :end_users
+  get 'items/index'
+  get 'items/top'
+  get 'items/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
