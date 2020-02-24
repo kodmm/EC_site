05b013956d18869ba1af090ea7cf0bac2455d70b
kodmm <tawaratsumida.kosei.infratop@gmail.com>
2020-02-24 04:29:53 +0000

    created Cart_item controller && model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'cart_items/index'
   root 'items#top'
+  get '/end_user/mypage'=> 'end_users#show', as: 'mypage_end_user'
   devise_for :admins
   devise_for :end_users
   resources :admins, only:[:show, :index, :edit]
-  resources :end_users, only:[:show, :edit, :confirm]
+  resource :end_users, only:[:edit, :confirm, :update]
   resources :items, only:[:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
