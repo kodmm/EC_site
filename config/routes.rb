de5e1ecea68a801efa2681e99600562274304f1d
kodmm <tawaratsumida.kosei.infratop@gmail.com>
2020-02-25 08:09:34 +0000

    fixed routing & created mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,29 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :end_users, controllers: {
+    sessions:      'end_users/sessions',
+    passwords:     'end_users/passwords',
+    registrations: 'end_users/registrations'
+  }
+  scope :admins do
+    resources :items, only:[:index, :show]
+  end
+
+  scope :end_users do
+    resources :items, only:[:index, :show]
+
+  end
+
+  resource :end_user, only:[:edit, :confirm, :update]
   root 'items#top'
 
   get '/end_user/mypage'=> 'end_users#show', as: 'mypage_end_user'
   get '/about' => 'abouts#about', as: 'about'
-  devise_for :admins
-  devise_for :end_users
   resources :admins, only:[:show, :index, :edit]
-  resource :end_users, only:[:edit, :confirm, :update]
-  resources :items, only:[:index, :show]
   resources :cart_items, only:[:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
