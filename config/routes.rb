8418576e667fc1b63aadd1b50981f9b1e4d18c83
kodmm <tawaratsumida.kosei.infratop@gmail.com>
2020-02-29 07:46:41 +0000

    almost completed end_users

--- config/routes.rb
+++ config/routes.rb
@@ -14,14 +14,16 @@ Rails.application.routes.draw do
   end
 
   namespace :end_users do
-    resources :addresses, only:[:create, :edit, :show]
+    
     resources :cart_items, only:[:index]
     resources :items, only:[:index, :show]
     get 'items/top'
 
     get 'end_users/confirm'
   end
+  
   scope module: :end_users do
+    resources :addresses, only:[:create, :edit, :show,:update, :destroy]
     resource :end_user, only:[:index, :edit, :confirm, :update]
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_063521) do
+ActiveRecord::Schema.define(version: 2020_02_29_065618) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "end_user_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_02_27_063521) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "postal_code"
   end
 
   create_table "admins", force: :cascade do |t|
