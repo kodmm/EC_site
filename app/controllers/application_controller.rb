dffb53361693a41a4848c372cf209e90d2fe6168
ko1028 <51409851+kodmm@users.noreply.github.com>
2020-03-02 21:08:00 +0900

    add sign_out_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,12 @@ class ApplicationController < ActionController::Base
         end
     end
     
-    def after_sign_out_path_for(resource)
-        root_path
+    def after_sign_out_path_for(resource_or_scope)
+        if resource_or_scope == Admin
+            new_admin_session_path
+        else
+            new_end_user_session_path
+        end
     end
 
 
