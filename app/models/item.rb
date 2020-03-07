06684d133513f7b001f775b60b6c21cf76920bdd
kodmm <tawaratsumida.kosei.infratop@gmail.com>
2020-03-07 05:38:32 +0000

    fixed Association

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
     enum status: {"販売中": 0, "品切れ": 1}
     attachment :image
     has_many :cart_items, dependent: :destroy
-    has_many :genres
+    belongs_to :genre
 
     validates :genre_id, presence: true
     validates :name, presence: true
